a7fc6656a9798d94be4db6eaa659fbdfa90a5c2e
unknown <fmreda3@gmail.com>
2020-05-20 18:22:36 +0000

    Updates

---
+++ precache-manifest.a9fcff5b752c0e9cc47d7d20edc11a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80803d5788c2d4192a08609f3c427aa",
+    "url": "/new-project/index.html"
+  },
+  {
+    "revision": "56e84b41ea1c1ca52838",
+    "url": "/new-project/static/css/main.ef59bb03.chunk.css"
+  },
+  {
+    "revision": "9f1e7e47dd47ac01d5fc",
+    "url": "/new-project/static/js/2.80d48306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-project/static/js/2.80d48306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e84b41ea1c1ca52838",
+    "url": "/new-project/static/js/main.63142e98.chunk.js"
+  },
+  {
+    "revision": "4b5bcc5c1927837ef126",
+    "url": "/new-project/static/js/runtime-main.95d28f78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
